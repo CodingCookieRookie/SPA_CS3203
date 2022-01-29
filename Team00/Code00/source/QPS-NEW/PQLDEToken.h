8cf62e8ee58a416a597ae92a3db23102741e6f74
DESKTOP-NS4HHOV\User <bioluminescencecloud@gmail.com>
2022-01-30 01:19:59 +0800

    limit word count of each deString to 2

--- Team00/Code00/source/QPS-NEW/PQLDEToken.h
+++ Team00/Code00/source/QPS-NEW/PQLDEToken.h
@@ -34,7 +34,12 @@ public:
         size_t index = 0;
         size_t spaceIndex = 0;
         bool expectDesignEntity = true;
+        int count = 0;
         while (index != string::npos) {
+            if (count >= 2) {   // invalid, for now count should have ended at 2 - one DE one var
+                PQLDEToken newDEToken;
+                return newDEToken;
+            }
             index = deString.find_first_not_of(" ");
             cout << "index: " << index << "\n";
             spaceIndex = deString.find(" ");
@@ -49,6 +54,7 @@ public:
                 deToken.setSynonym(word);
                 expectDesignEntity = true;
             }
+            count++;
             if (spaceIndex == string::npos) break; // means that this is the last word
             deString = deString.substr(spaceIndex + 1);
         }
