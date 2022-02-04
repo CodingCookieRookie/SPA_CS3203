e2ddf5044398c4630856a50e6ef5a7b79df97a80
Johanna <johannagwan@gmail.com>
2022-02-04 18:03:52 +0800

    Add Parse_MultipleProceduresReadPrint_Successful test

--- Team00/Code00/UnitTesting/TestParser.cpp
+++ Team00/Code00/UnitTesting/TestParser.cpp
@@ -13,7 +13,7 @@ namespace UnitTesting
 	public:
 		TEST_METHOD(Parse_OneProcedureReadPrint_Successful)
 		{
-			const char* source = "   procedure proc \n "
+			const char* source = "   procedure proc  "
 				"{ read x1; print y123;  \n "
 				" read Y1Yy ; } ";
 			Parser parser(source);
@@ -38,6 +38,45 @@ namespace UnitTesting
 			Assert::AreEqual(size_t(2), printNode->getStmtNum());
 
 			ReadNode* readNode2 = (ReadNode*)statements[2];
+			Assert::AreEqual(std::string("Y1Yy"), readNode2->getVarName());
+			Assert::AreEqual(size_t(3), readNode2->getStmtNum());
+		}
+
+		TEST_METHOD(Parse_MultipleProceduresReadPrint_Successful)
+		{
+			const char* source = "   procedure proc1  "
+				"{ read x1; } "
+				" \n procedure proc2 { \n"
+				" print y123;  \n "
+				" read Y1Yy ; } ";
+			Parser parser(source);
+			SourceAST ast = parser.parse();
+			std::vector<ProcedureNode*> procNodes = ast.getRoot()->getProcedureNodes();
+
+			/* Test procedureNodes */
+			Assert::AreEqual(size_t(2), procNodes.size());
+			Assert::AreEqual(std::string("proc1"), procNodes[0]->getProcName());
+			Assert::AreEqual(std::string("proc2"), procNodes[1]->getProcName());
+
+			/* Test statements in stmtListNode1 */
+			StmtListNode* stmtListNode1 = procNodes[0]->getStmtListNode();
+			std::vector<StmtNode*> statements1 = stmtListNode1->getStmtNodes();
+			Assert::AreEqual(size_t(1), statements1.size());
+
+			ReadNode* readNode1 = (ReadNode*)statements1[0];
+			Assert::AreEqual(std::string("x1"), readNode1->getVarName());
+			Assert::AreEqual(size_t(1), readNode1->getStmtNum());
+
+			/* Test statements in stmtListNode2 */
+			StmtListNode* stmtListNode2 = procNodes[1]->getStmtListNode();
+			std::vector<StmtNode*> statements2 = stmtListNode2->getStmtNodes();
+			Assert::AreEqual(size_t(2), statements2.size());
+
+			PrintNode* printNode = (PrintNode*)statements2[0];
+			Assert::AreEqual(std::string("y123"), printNode->getVarName());
+			Assert::AreEqual(size_t(2), printNode->getStmtNum());
+
+			ReadNode* readNode2 = (ReadNode*)statements2[1];
 			Assert::AreEqual(std::string("Y1Yy"), readNode2->getVarName());
 			Assert::AreEqual(size_t(3), readNode2->getStmtNum());
 		}
