f5ff8b610884f5dc665c104795d7409a4064c366
Johanna <johannagwan@gmail.com>
2022-02-05 11:38:02 +0800

    Update include statements ordering following google cpp

--- Team00/Code00/UnitTesting/TestAST.cpp
+++ Team00/Code00/UnitTesting/TestAST.cpp
@@ -1,19 +1,16 @@
-#include "stdafx.h"
 #include "CppUnitTest.h"
+#include "stdafx.h"
 //#include "TNode.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTesting
-{
-	TEST_CLASS(TestAST)
-	{
-	public:
+namespace UnitTesting {
+	TEST_CLASS(TestAST) {
+public:
 
-		TEST_METHOD(TestMethod1)
-		{
-			//TNode T;
-			// TODO: Your test code here
-		}
+	TEST_METHOD(TestMethod1) {
+		//TNode T;
+		// TODO: Your test code here
+	}
 	};
 }
