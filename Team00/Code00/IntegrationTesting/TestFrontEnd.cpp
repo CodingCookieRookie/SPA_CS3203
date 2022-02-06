45d363eff8f7754fb8a9eb4332c4ee4b4d628f98
Donavan Lim <donavanlim28@gmail.com>
2022-02-06 21:22:56 +0800

    Merge branch 'qps/QPS-mergePKBentities' into qps/QPS-evaluator-w-PKB Conflicts resolved manually. # Conflicts: #	Team00/Code00/SPA/SPA.vcxproj #	Team00/Code00/SPA/SPA.vcxproj.filters #	Team00/Code00/UnitTesting/UnitTesting.vcxproj #	Team00/Code00/UnitTesting/UnitTesting.vcxproj.filters

--- Team00/Code00/IntegrationTesting/TestFrontEnd.cpp
+++ Team00/Code00/IntegrationTesting/TestFrontEnd.cpp
@@ -4,15 +4,14 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace IntegrationTesting
-{		
+{
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
 			// TODO: Your test code here
 		}
-
 	};
 }
