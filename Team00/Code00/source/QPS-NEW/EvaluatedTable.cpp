6c1a2b369d24df45d439812db4b11171782b40b1
Donavan Lim <donavanlim28@gmail.com>
2022-02-06 11:29:23 +0800

    Rewrote EvaluatedTable definition

--- Team00/Code00/source/QPS-NEW/EvaluatedTable.cpp
+++ Team00/Code00/source/QPS-NEW/EvaluatedTable.cpp
@@ -1,2 +1,11 @@
 #include "EvaluatedTable.h"
 
+EvaluatedTable::EvaluatedTable(const EvaluatedTable& anotherEvTable) {
+	entities = anotherEvTable.entities;
+	numRow = anotherEvTable.numRow;
+	table = new std::unordered_map<PQL_VARIABLE_TYPE, std::vector<VALUE>>();
+	*table = *anotherEvTable.table
+}
+
+EvaluatedTable::EvaluatedTable(std::unordered_map<PQL_VARIABLE_TYPE, std::vector<VALUE>>* table) {
+}
--- Team00/Code00/source/QPS-NEW/EvaluatedTable.h
+++ Team00/Code00/source/QPS-NEW/EvaluatedTable.h
@@ -12,15 +12,42 @@ typedef std::string VALUE;
 class EvaluatedTable {
 private:
 	std::unordered_set<PQL_VARIABLE_TYPE> entities;
-	std::unordered_map<PQL_VARIABLE_TYPE, std::vector<VALUE>> table;
+	std::unordered_map<PQL_VARIABLE_TYPE, std::vector<VALUE>> *table;
 	int numRow;
 
 public:
 
+	/* For 
+	* {"s", "v"} = {{"1", "a"}, {"2", "b"}}
+	* EvalautedTable.entities = {"s", "v"} 
+	* EvalautedTable.*table = {
+		{"s", {"1", "2"}},
+	*	{"v", {"a", "b"}}
+	* }
+	*/
+
 	/* Default constructor */
-	EvaluatedTable();
+	EvaluatedTable() {
+		numRow = 0;
+	}
 
 	/* Wrapper constructor */
-	EvaluatedTable(const EvaluatedTable& anotherEvaluatedTable);
+	EvaluatedTable(const EvaluatedTable& anotherEvTable);
+
+	/** Construct based on *table only 
+	* 
+	* e.g.
+	* Given this from the PKB:
+	* {"s", "v"} = {{"1", "a"}, {"2", "b"}}
+	* 
+	* We construct this:
+	* EvaluatedTable(new std::unordered_map<PQL_VARIABLE_TYPE, std::vector<VALUE>>({
+	*	{"s", {"1", "2"}},
+	*	{"v", {"a", "b"}}
+	* }))
+	* 
+	* 
+	*/
+	EvaluatedTable(std::unordered_map<PQL_VARIABLE_TYPE, std::vector<VALUE>> *table);
 
 };
