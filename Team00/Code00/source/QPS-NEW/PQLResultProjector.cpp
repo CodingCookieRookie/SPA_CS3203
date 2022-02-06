68bd058fb7924e0db11a06539715b198a63fdd06
DESKTOP-NS4HHOV\User <bioluminescencecloud@gmail.com>
2022-02-06 17:33:11 +0800

    Remove some dependencies

--- Team00/Code00/source/QPS-NEW/PQLResultProjector.cpp
+++ Team00/Code00/source/QPS-NEW/PQLResultProjector.cpp
@@ -7,5 +7,5 @@ std::list<std::string> resolveTable(EvaluatedTable evaluatedTable) {
 	// we kind of need to check whether we want the entities or values or both to be returned right?
 	// Maybe we can add this state in evaluatedTable so evaluatedTable only contains what we want?
 	
-	//std::unordered_set<string> entities = evaluatedTable.getEvaluatedTable()
+	//std::unordered_set<std::string> entities = evaluatedTable.getEvaluatedTable();
 }
--- Team00/Code00/source/QPS-NEW/PQLResultProjector.h
+++ Team00/Code00/source/QPS-NEW/PQLResultProjector.h
@@ -1,6 +1,4 @@
 #pragma once
-#include <list>
-#include <string>
 #include "EvaluatedTable.h"
 
 class PQLResultProjector
