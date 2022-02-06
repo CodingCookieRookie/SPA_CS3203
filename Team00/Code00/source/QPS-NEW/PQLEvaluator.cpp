8ba2d2e78e46ff57f2044bc56c9ca5f4c6899716
Donavan Lim <donavanlim28@gmail.com>
2022-02-06 16:50:30 +0800

    1. Addded Instruction files to modularise Instruction as part of Command Pattern to PKB. 2. Did up logic for evaluateToInstructions().

--- Team00/Code00/source/QPS-NEW/PQLEvaluator.cpp
+++ Team00/Code00/source/QPS-NEW/PQLEvaluator.cpp
@@ -1,31 +1,46 @@
 #include "PQLEvaluator.h"
+#include "QPSCommons.h"
+#include "Instruction.h"
 
 #include <string>
 #include <optional>
 
-EvaluatedTable evaluate(ParsedQuery parsedQuery) {
+
+EvaluatedTable PQLEvaluator::evaluate(ParsedQuery parsedQuery) {
 	
-	std::vector<INSTRUCTION> instructions = evalauteToInstructions(parsedQuery);
+	std::vector<Instruction> instructions = PQLEvaluator::evaluateToInstructions(parsedQuery);
 	
 	EvaluatedTable resultEvTable = EvaluatedTable();
-	for (INSTRUCTION instruction : instructions) {
+	for (Instruction instruction : instructions) {
 		EvaluatedTable currEvTable = executeInstruction(instruction);
-		if !(resultEvTable.entities) {
+		if (resultEvTable.getEntities().empty()) {
 			resultEvTable = currEvTable;
 		} else {
-			resultEvTable.innerJoinMerge(currEvTable);
+			resultEvTable = innerJoinMerge(resultEvTable, currEvTable);
 		}
 	}
 	return resultEvTable;
 }
 
-std::vector<INSTRUCTION> evalauteToInstructions(ParsedQuery& pq) {
-	std::vector<INSTRUCTION> instructions;
-	// 1. Checks for what is required in the Select clause
+std::vector<Instruction> PQLEvaluator::evaluateToInstructions(ParsedQuery& pq) {
+	std::vector<Instruction> instructions = std::vector<Instruction>();
+	std::unordered_map<std::string, PQL_VARIABLE_TYPE> declarations = pq.getDeclarations();
 	std::vector<std::string> columns = pq.getColumns();
+	
+	// 1. Check if entitiy in Select clause is found in declarations
 	for (std::string entity : columns) {
-		// Match entity to 
+		PQL_VARIABLE_TYPE entityTypeRequired = declarations.at(entity);
+
+		switch (entityTypeRequired) {
+		case PQL_VARIABLE_TYPE::STMT :
+			instructions.push_back(Instruction(INSTRUCTION_TYPE::getAllStmt));
+			break;
+		case PQL_VARIABLE_TYPE::ASSIGN:
+			instructions.push_back(Instruction(INSTRUCTION_TYPE::getAllAsgn));
+			break;
+		}
 	}
+
 	// 2. If there are relations
 	//		checks for what relations are included in the ParsedQuery
 	//		Add respective instructions
@@ -34,16 +49,17 @@ std::vector<INSTRUCTION> evalauteToInstructions(ParsedQuery& pq) {
 	//		Add respective instructions
 
 	//TODO: Optimisation: Sort instructions.
+	return instructions;
 }
 
-EvaluatedTable executeInstruction(INSTRUCTION instruction) {
+EvaluatedTable executeInstruction(Instruction instruction) {
 	EvaluatedTable PKBresults = EvaluatedTable();
 	// Call relevant API
 
 	return PKBresults;
 }
 
-void innerJoinMerge(EvaluatedTable& newEvTable) {
+EvaluatedTable PQLEvaluator::innerJoinMerge(EvaluatedTable& evTable, EvaluatedTable& newEvTable) {
 	// 1. Short-circuit if other table is empty
-	// 2. 
+	// 2.
 }
