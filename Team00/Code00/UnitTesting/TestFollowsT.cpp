05434ebdb98ae718d7bf6e9a664b6f3d6252959b
arjavibahety <arjavibahety@gmail.com>
2022-02-08 20:00:26 +0800

    Add tests for FollowsT

--- Team00/Code00/UnitTesting/TestFollowsT.cpp
+++ Team00/Code00/UnitTesting/TestFollowsT.cpp
@@ -22,7 +22,7 @@ private:
 	}
 
 public:
-	TEST_METHOD(generate_getSuccessorStmts_branched) {
+	TEST_METHOD(populate_getSuccessorStmts_branched) {
 		std::unordered_set<StmtIndex, StmtIndex::HashFunction> followsTExpAns;
 		followsTExpAns.insert(stmt2);
 		followsTExpAns.insert(stmt3);
@@ -37,7 +37,7 @@ public:
 		Follows::insert(stmt2, stmt3);
 		Follows::insert(stmt2, stmt4);
 		Follows::insert(stmt1, stmt5);
-		FollowsT::generate();
+		FollowsT::populate();
 
 		auto followsTStmts = FollowsT::getSuccessorStmts(stmt1);
 		Assert::IsTrue(followsTExpAns == followsTStmts);
@@ -48,7 +48,7 @@ public:
 		/*TODO: When parent code is added, check it does not get affected*/
 	};
 
-	TEST_METHOD(generate_getSuccessorStmts_linear) {
+	TEST_METHOD(populate_getSuccessorStmts_linear) {
 		std::unordered_set<StmtIndex, StmtIndex::HashFunction> followsTExpAns;
 		followsTExpAns.insert(stmt1);
 		followsTExpAns.insert(stmt2);
@@ -62,7 +62,7 @@ public:
 		Follows::insert(stmt2, stmt3);
 		Follows::insert(stmt3, stmt4);
 		Follows::insert(stmt5, stmt1);
-		FollowsT::generate();
+		FollowsT::populate();
 
 		auto followsTStmts = FollowsT::getSuccessorStmts(stmt5);
 		Assert::IsTrue(followsTExpAns == followsTStmts);
@@ -72,5 +72,145 @@ public:
 
 		/*TODO: When parent code is added, check it does not get affected*/
 	};
+
+	TEST_METHOD(populate_getPredecessorStmts_branched) {
+		std::unordered_set<StmtIndex, StmtIndex::HashFunction> followsTExpAns;
+		followsTExpAns.insert(stmt1);
+		followsTExpAns.insert(stmt2);
+		followsTExpAns.insert(stmt4);
+
+		std::unordered_set<StmtIndex, StmtIndex::HashFunction> followsExpAns;
+		followsExpAns.insert(stmt4);
+
+		Follows::insert(stmt1, stmt2);
+		Follows::insert(stmt2, stmt3);
+		Follows::insert(stmt2, stmt4);
+		Follows::insert(stmt4, stmt5);
+		FollowsT::populate();
+
+		auto followsTStmts = FollowsT::getPredecessorStmts(stmt5);
+		Assert::IsTrue(followsTExpAns == followsTStmts);
+
+		auto followsStmts = Follows::getPredecessorStmts(stmt5);
+		Assert::IsTrue(followsExpAns == followsStmts);
+
+		/*TODO: When parent code is added, check it does not get affected*/
 	};
-}
+
+	TEST_METHOD(populate_getPredecessorStmts_linear) {
+		std::unordered_set<StmtIndex, StmtIndex::HashFunction> followsTExpAns;
+		followsTExpAns.insert(stmt1);
+		followsTExpAns.insert(stmt2);
+		followsTExpAns.insert(stmt3);
+		followsTExpAns.insert(stmt5);
+
+		std::unordered_set<StmtIndex, StmtIndex::HashFunction> followsExpAns;
+		followsExpAns.insert(stmt3);
+
+		Follows::insert(stmt1, stmt2);
+		Follows::insert(stmt2, stmt3);
+		Follows::insert(stmt3, stmt4);
+		Follows::insert(stmt5, stmt1);
+		FollowsT::populate();
+
+		auto followsTStmts = FollowsT::getPredecessorStmts(stmt4);
+		Assert::IsTrue(followsTExpAns == followsTStmts);
+
+		auto followsStmts = Follows::getPredecessorStmts(stmt4);
+		Assert::IsTrue(followsExpAns == followsStmts);
+
+		/*TODO: When parent code is added, check it does not get affected*/
+	};
+
+	TEST_METHOD(containsSuccessor) {
+		Follows::insert(stmt1, stmt2);
+		Follows::insert(stmt2, stmt3);
+		Follows::insert(stmt2, stmt4);
+		FollowsT::populate();
+
+		Assert::IsTrue(FollowsT::containsSuccessor(stmt1, stmt4));
+		Assert::IsFalse(FollowsT::containsSuccessor(stmt4, stmt1));
+		Assert::IsFalse(FollowsT::containsSuccessor(stmt3, stmt4)); /*siblings*/
+
+		Assert::IsTrue(Follows::containsSuccessor(stmt1, stmt2));
+		Assert::IsFalse(Follows::containsSuccessor(stmt1, stmt4));
+	};
+
+	TEST_METHOD(containsPredecessor) {
+		Follows::insert(stmt1, stmt2);
+		Follows::insert(stmt2, stmt3);
+		Follows::insert(stmt2, stmt4);
+		FollowsT::populate();
+
+		Assert::IsTrue(FollowsT::containsPredecessor(stmt1, stmt4));
+		Assert::IsFalse(FollowsT::containsPredecessor(stmt4, stmt1));
+		Assert::IsFalse(FollowsT::containsPredecessor(stmt3, stmt4)); /*siblings*/
+
+		Assert::IsTrue(Follows::containsPredecessor(stmt1, stmt2));
+		Assert::IsFalse(Follows::containsPredecessor(stmt1, stmt4));
+	};
+
+	TEST_METHOD(getAllPredecessorSuccessorInfo) {
+		std::vector<std::tuple<StmtIndex, StmtIndex>> followsTExpAns;
+		followsTExpAns.push_back(std::make_tuple(stmt1, stmt2));
+		followsTExpAns.push_back(std::make_tuple(stmt1, stmt3));
+		followsTExpAns.push_back(std::make_tuple(stmt2, stmt3));
+
+		std::vector<std::tuple<StmtIndex, StmtIndex>> followsExpAns;
+		followsExpAns.push_back(std::make_tuple(stmt1, stmt2));
+		followsExpAns.push_back(std::make_tuple(stmt2, stmt3));
+
+		Follows::insert(stmt1, stmt2);
+		Follows::insert(stmt2, stmt3);
+		FollowsT::populate();
+
+		auto followsTInfo = FollowsT::getAllPredecessorSuccessorInfo();
+		auto followsInfo = Follows::getAllPredecessorSuccessorInfo();
+
+		Assert::IsTrue(followsTExpAns == followsTInfo);
+		Assert::IsTrue(followsExpAns == followsInfo);
+	};
+
+	TEST_METHOD(getPredSucTable) {
+		std::unordered_map<StmtIndex, std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> followsTExpAns;
+		followsTExpAns[stmt1].insert(stmt2);
+		followsTExpAns[stmt1].insert(stmt3);
+		followsTExpAns[stmt2].insert(stmt3);
+
+		std::unordered_map<StmtIndex, std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> followsExpAns;
+		followsExpAns[stmt1].insert(stmt2);
+		followsExpAns[stmt2].insert(stmt3);
+
+		Follows::insert(stmt1, stmt2);
+		Follows::insert(stmt2, stmt3);
+		FollowsT::populate();
+
+		auto followsTTable = FollowsT::getPredSucTable();
+		auto followsTable = Follows::getPredSucTable();
+
+		Assert::IsTrue(followsTExpAns == followsTTable);
+		Assert::IsTrue(followsExpAns == followsTable);
+	};
+
+	TEST_METHOD(getSucPredTable) {
+		std::unordered_map<StmtIndex, std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> followsTExpAns;
+		followsTExpAns[stmt2].insert(stmt1);
+		followsTExpAns[stmt3].insert(stmt1);
+		followsTExpAns[stmt3].insert(stmt2);
+
+		std::unordered_map<StmtIndex, std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> followsExpAns;
+		followsExpAns[stmt2].insert(stmt1);
+		followsExpAns[stmt3].insert(stmt2);
+
+		Follows::insert(stmt1, stmt2);
+		Follows::insert(stmt2, stmt3);
+		FollowsT::populate();
+
+		auto followsTTable = FollowsT::getSucPredTable();
+		auto followsTable = Follows::getSucPredTable();
+
+		Assert::IsTrue(followsTExpAns == followsTTable);
+		Assert::IsTrue(followsExpAns == followsTable);
+	};
+	};
+};
