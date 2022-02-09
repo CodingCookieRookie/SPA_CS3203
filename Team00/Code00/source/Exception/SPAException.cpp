5fbeb0910d0cea69d8689c3e39d62ecbf10c2956
Johanna <johannagwan@gmail.com>
2022-02-10 02:00:08 +0800

    Remove . in exception msg

--- Team00/Code00/source/Exception/SPAException.cpp
+++ Team00/Code00/source/Exception/SPAException.cpp
@@ -12,8 +12,8 @@ const std::string LexerException::INVALID_INT = "Integer cannot start with 0";
 LexerException::LexerException(const std::string& reason) : SPAException(reason) {}
 
 /* ParserException */
-const std::string ParserException::MISSING_SEMICOLON = "Semicolon is missing.";
-const std::string ParserException::MISSING_VAR_NAME = "Variable name is missing.";
-const std::string ParserException::MISSING_WHITESPACE = "Whitespace is missing.";
+const std::string ParserException::MISSING_SEMICOLON = "Semicolon is missing";
+const std::string ParserException::MISSING_VAR_NAME = "Variable name is missing";
+const std::string ParserException::MISSING_WHITESPACE = "Whitespace is missing";
 
 ParserException::ParserException(const std::string& reason) : SPAException(reason) {}
