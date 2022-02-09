b7b31d904eeeef91d95ebfd0903900c1dc6c52aa
Donavan Lim <donavanlim28@gmail.com>
2022-02-09 13:26:01 +0800

    Removed duplicate line

--- Team00/Code00/source/QPS-NEW/PQLEvaluator.cpp
+++ Team00/Code00/source/QPS-NEW/PQLEvaluator.cpp
@@ -92,7 +92,7 @@ EvaluatedTable PQLEvaluator::execute(Instruction& instr) {
        /* currTable = EvaluatedTable(std::unordered_set<PqlEntityType>({ PqlEntityType::Stmt }),
             std::unordered_map<PqlEntityType, std::vector<VALUE>>({ {PqlEntityType::Stmt, resultsToStr} }),
             results.size());*/
-        std::vector<StmtIndex> results = Entity::getAllStmts();
+
         std::vector<VALUE> resultsToStr;
         std::unordered_set<PqlEntityType> PQLtypes;
         PQLtypes.insert(PqlEntityType::Stmt);
