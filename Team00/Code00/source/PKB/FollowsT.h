d666b649f1bc0d0e582aaca8d6e50ef9895b9fce
arjavibahety <arjavibahety@gmail.com>
2022-02-13 14:24:54 +0800

    Refactor FollowsT to use RS2T interface

--- Team00/Code00/source/PKB/FollowsT.h
+++ Team00/Code00/source/PKB/FollowsT.h
@@ -3,15 +3,9 @@
 #ifndef FOLLOWST_H
 #define FOLLOWST_H
 
-#include "./RS2.h"
+#include "./RS2T.h"
 #include "./Follows.h"
 
-class FollowsT : public RS2<FollowsT> {
-private:
-	static void insert(StmtIndex predecessor, std::unordered_set<StmtIndex, StmtIndex::HashFunction> successors);
-	static std::unordered_set<StmtIndex, StmtIndex::HashFunction> getAllSuccessors(StmtIndex predecessor);
-public:
-	static void populate();
-};
+class FollowsT : public RS2T<FollowsT, Follows> {};
 
 #endif
