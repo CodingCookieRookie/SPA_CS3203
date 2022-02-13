e15a7ddb34a4cc0b003b28f53e0d09ecd9d39c1a
rolandyuwy <rolandyuwenyang@hotmail.com>
2022-02-13 21:45:20 +0800

    Add header API

--- Team00/Code00/source/PKB/Entity.h
+++ Team00/Code00/source/PKB/Entity.h
@@ -27,16 +27,19 @@ protected:
 
 public:
 	static VarIndex insertVar(std::string varName);
+	static bool containsVar(std::string varName);
 	static std::string getVarName(VarIndex varIdx);
 	static VarIndex getVarIdx(std::string varName);
 	static std::vector<std::string> getAllVars();
 	static ProcIndex insertProc(std::string procName);
+	static bool containsProc(std::string procName);
 	static std::string getProcName(ProcIndex procIdx);
 	static ProcIndex getProcIdx(std::string procName);
 	static std::vector<std::string> getAllProcs();
 	static void insertConst(int constant);
 	static std::vector<int> getAllConsts();
 	static StmtIndex insertStmt(StatementType stmtType);
+	static bool containsStmt(int stmtNo);
 	static std::unordered_set<StmtIndex, StmtIndex::HashFunction> getStmtIdxFromType(StatementType stmtType);
 	static std::vector<StmtIndex> getAllStmts();
 	static void insertStmtFromProc(ProcIndex procIdx, StmtIndex stmtIdx);
