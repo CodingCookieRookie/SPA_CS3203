9db424aeab22651c02bb271ae814cfdc7bc018e0
arjavibahety <arjavibahety@gmail.com>
2022-02-13 14:58:46 +0800

    Add parent and parentT code

---
+++ Team00/Code00/source/PKB/Parent.cpp
@@ -0,0 +1,6 @@
+#include "./Parent.h"
+
+void Parent::insert(StmtIndex  predecessor, StmtIndex successor) {
+	predSucTable[predecessor].insert(successor);
+	sucPredTable[successor].insert(predecessor);
+};
---
+++ Team00/Code00/source/PKB/ParentT.cpp
@@ -0,0 +1,1 @@
+#include "./ParentT.h"
