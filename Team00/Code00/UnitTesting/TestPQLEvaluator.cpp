a67fe81555cf0d8c1b5b34d260019f37a6a41915
Donavan Lim <donavanlim28@gmail.com>
2022-02-17 00:50:21 +0800

    Added Test case for Follows(1, s2)

--- Team00/Code00/UnitTesting/TestPQLEvaluator.cpp
+++ Team00/Code00/UnitTesting/TestPQLEvaluator.cpp
@@ -41,7 +41,7 @@ namespace UnitTesting
             Assert::AreEqual(std::string("s"), pq1.getColumns()[0]);
         }
 
-        TEST_METHOD(evaluateQuery_followsOnly_noMerge_designEntitiesExtracted)
+        TEST_METHOD(executeInstruction_follows_twoConstants)
         {
  
             // Setup: Follows(1, 2) RelationshipInstruction
@@ -51,9 +51,9 @@ namespace UnitTesting
             Instruction* instruction = new RelationshipInstruction(PqlRelationshipType::Follows, lhsRef, rhsRef);
 
             // Setup: PKB insert statements
-            StmtIndex s1 = Entity::insertStmt(StatementType::assignType);
-            StmtIndex s2 = Entity::insertStmt(StatementType::assignType);
-            Follows::insert(s1, s2);
+            StmtIndex stmt1 = Entity::insertStmt(StatementType::assignType);
+            StmtIndex stmt2 = Entity::insertStmt(StatementType::assignType);
+            Follows::insert(stmt1, stmt2);
 
             // Main test:
             EvaluatedTable evTable = instruction->execute();
@@ -66,6 +66,51 @@ namespace UnitTesting
             Follows::performCleanUp();
         }
 
-        std::vector<Instruction*> instructions = std::vector<Instruction*>();
+        TEST_METHOD(executeInstruction_follows_lhsConstrhsStmt)
+        {
+
+            // 1. Setup:
+            // Follows(1, s2) RelationshipInstruction
+            PqlReference lhsRef, rhsRef;
+            lhsRef = std::make_pair(PqlReferenceType::integer, "1");
+            rhsRef = std::make_pair(PqlReferenceType::synonym, "s2");
+            Instruction* instruction = new RelationshipInstruction(PqlRelationshipType::Follows, lhsRef, rhsRef);
+
+            // PKB insert statements
+            StmtIndex stmt1 = Entity::insertStmt(StatementType::assignType);
+            StmtIndex stmt2 = Entity::insertStmt(StatementType::assignType);
+            Follows::insert(stmt1, stmt2);
+
+            // 2. Main test:
+            EvaluatedTable evTable = instruction->execute();
+
+            // Test numRow:
+            Assert::AreEqual(size_t(1), evTable.getNumRow());
+
+            // Test Table: std::unordered_map<std::string, std::vector<int>>
+            auto tableRef = evTable.getTableRef();
+            Assert::AreEqual(true, tableRef.find("s2") != tableRef.end());
+            Assert::AreEqual(false, tableRef.find("s3") != tableRef.end());
+
+            // Test Entities: std::unordered_map<std::string, PqlEntityType>
+            std::vector<int> values{ 2 };
+            auto actualValues = tableRef.at("s2");
+            bool areVecEqual = std::equal(values.begin(), values.end(), actualValues.begin());
+            Assert::AreEqual(true, areVecEqual);
+            auto actualEntities = evTable.getEntities();
+            Assert::AreEqual(true, actualEntities.find("s2") != actualEntities.end());
+            Assert::AreEqual(false, actualEntities.find("s3") != actualEntities.end());
+            bool isPqlEntityType = PqlEntityType::Stmt == actualEntities.at("s2");
+            Assert::AreEqual(true, isPqlEntityType);
+
+            // Test EvResult:
+            bool actualEvResult = evTable.getEvResult();
+            Assert::AreEqual(true, actualEvResult);
+
+            // 3. Clean-up:
+            Entity::performCleanUp();
+            Follows::performCleanUp();
+        }
+
     };
 }
