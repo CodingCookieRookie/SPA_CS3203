1f7540927748286d83ba8c61b295dbbcf8866f4d
Donavan Lim <donavanlim28@gmail.com>
2022-02-18 16:21:51 +0800

    Stress test 'Select pn1'

--- Team00/Code00/UnitTesting/TestGetAllInstruction.cpp
+++ Team00/Code00/UnitTesting/TestGetAllInstruction.cpp
@@ -20,7 +20,7 @@ namespace UnitTesting
         }
     public:
 
-        TEST_METHOD(executeGetAllInstruction_twoConstants_evaluatedTableFormed) {
+        TEST_METHOD(executeGetAllInstruction_getAllStmt_evaluatedTableFormed) {
 
             // 1. Setup:
             // The 'Select s1' portion of the query
@@ -31,13 +31,13 @@ namespace UnitTesting
             for (int i = 0; i < 5; i++) {
                 stmts.emplace_back(Entity::insertStmt(StatementType::assignType));
             }
-            
+
             // 2. Main test:
             EvaluatedTable evTable = instruction->execute();
 
             // Test numRow:
             Assert::AreEqual(size_t(5), evTable.getNumRow());
-            
+
             // Test Table: std::unordered_map<std::string, std::vector<int>>
             auto tableRef = evTable.getTableRef();
             Assert::AreEqual(true, tableRef.find("s1") != tableRef.end());
@@ -46,6 +46,52 @@ namespace UnitTesting
             std::vector<int> values{ 1, 2, 3, 4, 5 };
             auto actualValues = tableRef.at("s1");
             bool areVecEqual = std::equal(values.begin(), values.end(), actualValues.begin());
+            Assert::AreEqual(true, areVecEqual);
+
+            // Test EvResult:
+            bool actualEvResult = evTable.getEvResult();
+            Assert::AreEqual(true, actualEvResult);
+
+            // 3. Clean-up:
+            Entity::performCleanUp();
+            Parent::performCleanUp();
+        }
+
+        TEST_METHOD(executeGetAllInstruction_getAllPrintsStress_evaluatedTableFormed) {
+
+            // 1. Setup:
+            // The 'Select pn1' portion of the query
+            Instruction* instruction = new GetAllInstruction(PqlEntityType::Print, "pn1");
+
+            // PKB inserts 98 statements
+            std::vector<StmtIndex> stmts;
+            for (int i = 0; i < 49; i++) {
+                stmts.emplace_back(Entity::insertStmt(StatementType::printType));
+            }
+            for (int i = 0; i < 50; i++) {
+                stmts.emplace_back(Entity::insertStmt(StatementType::assignType)); // extra
+            }
+
+            // 2. Main test:
+            EvaluatedTable evTable = instruction->execute();
+
+            // Test numRow:
+            Assert::AreEqual(size_t(49), evTable.getNumRow());
+
+            // Test Table: std::unordered_map<std::string, std::vector<int>>
+            auto tableRef = evTable.getTableRef();
+            Assert::AreEqual(true, tableRef.find("pn1") != tableRef.end());
+
+            // Test Entities: std::unordered_map<std::string, PqlEntityType>
+            std::vector<int> pn1values;
+            for (int i = 0; i < 49; i++) {
+                pn1values.emplace_back(i + 1);
+            }
+            auto actualValues = tableRef.at("pn1");
+            // Cannot check equality due to unordering (actuals1Values is not {1, 2, ..., 49}), this is PKB's optimisation.
+            // Instead sort, and check for equality
+            std::sort(actualValues.begin(), actualValues.end());
+            bool areVecEqual = std::equal(pn1values.begin(), pn1values.end(), actualValues.begin());
             Assert::AreEqual(true, areVecEqual);
 
             // Test EvResult:
