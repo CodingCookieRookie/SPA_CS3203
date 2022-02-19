2def621d977565a6678a09d3c8a6ff8fecd20325
Donavan Lim <donavanlim28@gmail.com>
2022-02-19 12:54:56 +0800

    Arjavi fixed dynamic programmic issues for ParentT::populate() Fixed test cases by sorting every vector<value> returned

--- Team00/Code00/UnitTesting/TestInstructionFollowsT.cpp
+++ Team00/Code00/UnitTesting/TestInstructionFollowsT.cpp
@@ -202,10 +202,12 @@ namespace UnitTesting
             // Test Entities: std::unordered_map<std::string, PqlEntityType>
             std::vector<int> s1values{ 1, 1, 1, 2, 2, 3 };
             auto actuals1Values = tableRef.at("s1");
+            std::sort(actuals1Values.begin(), actuals1Values.end());
             bool areVecEqual = std::equal(s1values.begin(), s1values.end(), actuals1Values.begin());
             Assert::AreEqual(true, areVecEqual);
-            std::vector<int> s2values{ 2, 3, 4, 3, 4, 4 };
+            std::vector<int> s2values{ 2, 3, 3, 4, 4, 4 };
             auto actuals2Values = tableRef.at("s2");
+            std::sort(actuals2Values.begin(), actuals2Values.end());
             bool areVecEqual2 = std::equal(s2values.begin(), s2values.end(), actuals2Values.begin());
             Assert::AreEqual(true, areVecEqual2);
 
@@ -279,9 +281,12 @@ namespace UnitTesting
             }
 
             auto actuals1Values = tableRef.at("s1");
+            std::sort(actuals1Values.begin(), actuals1Values.end());
             bool areVecEqual = std::equal(s1values.begin(), s1values.end(), actuals1Values.begin());
             Assert::AreEqual(true, areVecEqual); // s1values == {19 1s, 18 2s, ... 1 19}
             auto actuals2Values = tableRef.at("s2");
+            std::sort(actuals2Values.begin(), actuals2Values.end());
+            std::sort(wildcardValues.begin(), wildcardValues.end());
             bool areVecEqual2 = std::equal(wildcardValues.begin(), wildcardValues.end(), actuals2Values.begin());
             Assert::AreEqual(true, areVecEqual2); // wildcardValues == {2-19, 3-19, ..., 18, 19, 19}
 
@@ -349,6 +354,7 @@ namespace UnitTesting
             }
 
             auto actuals1Values = tableRef.at("s1");
+            std::sort(actuals1Values.begin(), actuals1Values.end());
             bool areVecEqual = std::equal(s1values.begin(), s1values.end(), actuals1Values.begin());
             Assert::AreEqual(true, areVecEqual); // s1values == {19 1s, 18 2s, ... 1 19}
 
--- Team00/Code00/source/PKB/RS2T.h
+++ Team00/Code00/source/PKB/RS2T.h
@@ -8,14 +8,14 @@
 template<class T>
 class RS2T : public RS2<RS2T<T>> {
 private:
-	using RS2<RS2T>::insert;
-	static std::unordered_set<StmtIndex, StmtIndex::HashFunction> getAllSuccessors(StmtIndex predecessor, std::unordered_map<StmtIndex,
-		std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> uPredSucTable);
+    using RS2<RS2T>::insert;
+    static std::unordered_set<StmtIndex, StmtIndex::HashFunction> getAllSuccessors(StmtIndex predecessor, std::unordered_map<StmtIndex,
+        std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> uPredSucTable);
 protected:
-	RS2T();
+    RS2T();
 public:
-	static void populate(std::unordered_map<StmtIndex,
-		std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> uPredSucTable);
+    static void populate(std::unordered_map<StmtIndex,
+        std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> uPredSucTable);
 };
 
 template<class T>
@@ -23,33 +23,38 @@ RS2T<T>::RS2T() {};
 
 template<class T>
 std::unordered_set<StmtIndex, StmtIndex::HashFunction> RS2T<T>::getAllSuccessors(StmtIndex predecessor,
-	std::unordered_map<StmtIndex, std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> uPredSucTable) {
-	if (predSucTable.find(predecessor) != predSucTable.end()) {
-		return predSucTable[predecessor];
-	}
+    std::unordered_map<StmtIndex, std::unordered_set<StmtIndex, StmtIndex::HashFunction>, StmtIndex::HashFunction> uPredSucTable) {
+    if (predSucTable.find(predecessor) != predSucTable.end()) {
+        return predSucTable[predecessor];
+    }
 
-	std::unordered_set<StmtIndex, StmtIndex::HashFunction> successors;
-	for (auto& successor : uPredSucTable[predecessor]) {
-		successors.insert(successor);
-		std::unordered_set<StmtIndex, StmtIndex::HashFunction> grandSuccessors = getAllSuccessors(successor, uPredSucTable);
-		for (auto& grandSuccessor : grandSuccessors) {
-			successors.insert(grandSuccessor);
-		}
-	}
+    std::unordered_set<StmtIndex, StmtIndex::HashFunction> successors;
+    for (auto& successor : uPredSucTable[predecessor]) {
+        successors.insert(successor);
+        std::unordered_set<StmtIndex, StmtIndex::HashFunction> grandSuccessors = getAllSuccessors(successor, uPredSucTable);
+        for (auto& grandSuccessor : grandSuccessors) {
+            successors.insert(grandSuccessor);
+            insert(successor, grandSuccessor);
+        }
+    }
 
-	return successors;
+    return successors;
 }
 
 template<class T>
 void RS2T<T>::populate(std::unordered_map<StmtIndex, std::unordered_set<StmtIndex, StmtIndex::HashFunction>,
-	StmtIndex::HashFunction> uPredSucTable) {
-	for (auto& uPredSucEntry : uPredSucTable) {
-		StmtIndex predecessor = uPredSucEntry.first;
-		std::unordered_set<StmtIndex, StmtIndex::HashFunction> successors = getAllSuccessors(predecessor, uPredSucTable);
-		for (auto& successor : successors) {
-			insert(predecessor, successor);
-		}
-	}
+    StmtIndex::HashFunction> uPredSucTable) {
+    for (auto& uPredSucEntry : uPredSucTable) {
+        StmtIndex predecessor = uPredSucEntry.first;
+        if (predSucTable.find(predecessor) != predSucTable.end()) {
+            continue;
+        }
+
+        std::unordered_set<StmtIndex, StmtIndex::HashFunction> successors = getAllSuccessors(predecessor, uPredSucTable);
+        for (auto& successor : successors) {
+            insert(predecessor, successor);
+        }
+    }
 };
 
 #endif
