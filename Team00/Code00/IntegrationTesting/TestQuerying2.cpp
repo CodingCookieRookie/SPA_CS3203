02ec158e81e14fefe20966c5ceadaecb76075e19
DESKTOP-NS4HHOV\User <bioluminescencecloud@gmail.com>
2022-02-20 00:28:23 +0800

    Working on patternOnly

--- Team00/Code00/IntegrationTesting/TestQuerying2.cpp
+++ Team00/Code00/IntegrationTesting/TestQuerying2.cpp
@@ -144,54 +144,59 @@ namespace IntegrationTesting
 		{
 			// b = b + x
 			// 1. Setup:
-			//std::string query = "assign a1; variable v1; Select a1 pattern a1 (v1 , "_x_" )";
+			std::string query = "assign a; variable v; Select a pattern a(v, _\"x\"_)";
 			// PKB inserts 6 types of statements
-			//std::vector<StmtIndex> stmts;
-			//stmts.emplace_back(Entity::insertStmt(StatementType::assignType));
-			//stmts.emplace_back(Entity::insertStmt(StatementType::printType));
-			//stmts.emplace_back(Entity::insertStmt(StatementType::callType));
-			//stmts.emplace_back(Entity::insertStmt(StatementType::ifType));
-			//stmts.emplace_back(Entity::insertStmt(StatementType::whileType));
-			//stmts.emplace_back(Entity::insertStmt(StatementType::readType));
+			std::vector<StmtIndex> stmts;
+			stmts.emplace_back(Entity::insertStmt(StatementType::assignType));
+			stmts.emplace_back(Entity::insertStmt(StatementType::printType));
+			stmts.emplace_back(Entity::insertStmt(StatementType::callType));
+			stmts.emplace_back(Entity::insertStmt(StatementType::ifType));
+			stmts.emplace_back(Entity::insertStmt(StatementType::whileType));
+			stmts.emplace_back(Entity::insertStmt(StatementType::readType));
 
-			//// PKB inserts pattern
-			//StmtIndex stmt = Entity::insertStmt(StatementType::assignType);
-			//Entity::insertVar("a");
-			//VarIndex varIndex = Entity::insertVar("b");
-			//std::string postFixExpression = ExpressionProcessor::convertInfixToPostFix("b + x");
-			//Pattern::insertPostFixInfo(varIndex, postFixExpression, stmt);
+			// PKB inserts pattern
+			StmtIndex stmt = Entity::insertStmt(StatementType::assignType);
+			Entity::insertVar("a");
+			VarIndex varIndex = Entity::insertVar("b");
+			std::string postFixExpression = ExpressionProcessor::convertInfixToPostFix("b + x");
+			Pattern::insertPostFixInfo(varIndex, postFixExpression, stmt);
+
+			// Check PBK populated
+			std::tuple<std::vector<int>, std::vector<int>> allPatternStmtInfo = Pattern::getStmtsFromPattern("x", true);
+			Assert::AreEqual(size_t(1), std::get<0>(allPatternStmtInfo).size());
 
 			// 2. Test QPS Parser:
-			//ParsedQuery parsedQuery = PQLParser::parseQuery(query);
-			/*		Assert::AreEqual(size_t(2), parsedQuery.getDeclarations().size());
-					Assert::AreEqual(size_t(1), parsedQuery.getColumns().size());*/
+			ParsedQuery parsedQuery = PQLParser::parseQuery(query);
+			Assert::AreEqual(size_t(2), parsedQuery.getDeclarations().size());
+			Assert::AreEqual(size_t(1), parsedQuery.getColumns().size());
 
-					//// 3. Test QPS Evaluator:
-					//PQLEvaluator pqlEvaluator = PQLEvaluator(parsedQuery);
-					//EvaluatedTable evTable = pqlEvaluator.evaluate();
+			// 3. Test QPS Evaluator:
+			//PQLEvaluator pqlEvaluator = PQLEvaluator(parsedQuery);
+			//EvaluatedTable evTable = pqlEvaluator.evaluate();
 
-					//// Test numRow:
-					//Assert::AreEqual(size_t(2), evTable.getNumRow());
+			//// Test numRow:
+			//Assert::AreEqual(size_t(1), evTable.getNumRow());
 
-					// Test Table:
-					//auto tableRef = evTable.getTableRef();
-					//Assert::AreEqual(true, tableRef.find("a1") != tableRef.end());
+			//// Test Table:
+			//auto tableRef = evTable.getTableRef();
+			//Assert::AreEqual(true, tableRef.find("b") != tableRef.end());
 
-					//// Test Values: std::unordered_map<std::string, PqlEntityType>
-					//std::vector<int> values{ 7, 7 };
-					//auto actualValues = tableRef.at("a1");
-					//bool areVecEqual = std::equal(values.begin(), values.end(), actualValues.begin());
-					//Assert::AreEqual(true, areVecEqual);
+			//// Test Values: std::unordered_map<std::string, PqlEntityType>
+			//std::vector<int> values{ 7, 7 };
+			//auto actualValues = tableRef.at("b");
+			//bool areVecEqual = std::equal(values.begin(), values.end(), actualValues.begin());
+			//Assert::AreEqual(true, areVecEqual);
 
-					//// Test EvResult:
-					////bool actualEvResult = evTable.getEvResult();
-					////Assert::AreEqual(true, actualEvResult);
+			// Test EvResult:
+			//bool actualEvResult = evTable.getEvResult();
+			//Assert::AreEqual(true, actualEvResult);
 
-					//// 4. Test QPS Result Projector:
-					//PQLResultProjector resultProjector = PQLResultProjector(evTable, parsedQuery.getColumns());
-					//std::list<std::string> results = resultProjector.resolveTableToResults();
-					//std::list<std::string> expectedRes{ "7" };
-					//bool areListsEqual = std::equal(expectedRes.begin(), expectedRes.end(), results.begin());
-					//Assert::AreEqual(true, areListsEqual);
+			// 4. Test QPS Result Projector:
+			//PQLResultProjector resultProjector = PQLResultProjector(evTable, parsedQuery.getColumns());
+			//std::list<std::string> results = resultProjector.resolveTableToResults();
+			//std::list<std::string> expectedRes{ "7" };
+			//bool areListsEqual = std::equal(expectedRes.begin(), expectedRes.end(), results.begin());
+			//Assert::AreEqual(true, areListsEqual);
 		}
-	}
+	};
+}
