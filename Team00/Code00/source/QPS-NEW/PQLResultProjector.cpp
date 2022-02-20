f8fd825bd8fd1c7ab11d9caa3b3a53c56395433c
Brandon Foong <brandonfoongyankai@gmail.com>
2022-02-20 15:56:15 +0800

    Restructure query evaluation for optimization

--- Team00/Code00/source/QPS-NEW/PQLResultProjector.cpp
+++ Team00/Code00/source/QPS-NEW/PQLResultProjector.cpp
@@ -2,54 +2,69 @@
 #include "EvaluatedTable.h"
 #include "../PKB/Entity.h"
 #include "QPSCommons.h"
+#include "../QPS-NEW/Instruction.h"
 
 #include <string>
 #include <list>
 #include <unordered_set>
 
 std::list<std::string> PQLResultProjector::resolveTableToResults() {
-	std::unordered_map<std::string, PqlEntityType> entities = evaluatedTable.getEntities();
 	std::unordered_map<std::string, std::vector<int>> table = evaluatedTable.getTableRef();
-	int numRow = evaluatedTable.getNumRow();
 	std::list<std::string> resList;
+
+	std::unordered_map<std::string, PqlEntityType> resultEntities;
+	std::unordered_map<std::string, std::vector<int>> resultTable;
+	for (std::string& column : columnsProjected) {
+		/* For each column that already exists in the final EvTable, take it from the EvTable */
+		if (table.find(column) != table.end()) {
+			resultEntities[column] = declarations[column];
+			resultTable[column] = table[column];
+		}
+	}
+
+	/* If the evaluated table is false or an empty table, use a false table */
+	EvaluatedTable resultEvTable(resultEntities, resultTable);
+	if (evaluatedTable.getEvResult() == false
+		|| (table.size() > 0 && evaluatedTable.getNumRow() == 0)) {
+		resultEvTable = EvaluatedTable(false);
+	}
+	
+	for (std::string& column : columnsProjected) {
+		if (table.find(column) == table.end()) {
+			/* For each column that does not exist in the final EvTable, get it via an instruction 
+			   and then perform a cross product. */
+			PqlEntityType columnType = declarations.at(column);
+			Instruction* getAll = new GetAllInstruction(columnType, column);
+			EvaluatedTable evTable = getAll->execute();
+			resultEvTable = resultEvTable.innerJoinMerge(evTable);
+		}
+	}
+	/* Extract the table from the EvTable */
+	resultTable = resultEvTable.getTableRef();
+	int numRow = resultEvTable.getNumRow();
 
 	// 1. Projecting only selected columns
 	// E.g.
 	// { {STMT, {1, 3, 4}, {ASGN, {2, 2, 3}} },
 	// Forms {"1, 2", "3, 2", "4, 3"} or 1 2 3 2 4 3 according to AutoTester
 	for (int i = 0; i < numRow; i++) {	// for each row
-		std::unordered_map<std::string, std::vector<int>>::iterator it = table.begin();
 		std::string res = "";
-		while (it != table.end()) {	// for each col
-			std::string entityName = it->first;
-			std::string value;
-			// if column is not selected, do not project
-			if (std::find(columnsProjected.begin(), columnsProjected.end(), entityName) == columnsProjected.end()) {
-				it++;
-				continue;
+		bool firstCol = true;
+		for (std::string& entityName : columnsProjected) {
+			std::string value = "";
+			if (isStatementEntity(declarations[entityName])
+				|| declarations[entityName] == PqlEntityType::Constant) {
+				value = std::to_string(resultTable[entityName][i]);
+			} else if ((declarations[entityName] == PqlEntityType::Variable)) {
+				value = Entity::getVarName(resultTable[entityName][i]);
+			} else { //PqlEntityType::Procedure
+				value = Entity::getProcName(resultTable[entityName][i]);
 			}
-
-			if (it->second.size() == 0) {
-				break;
+			if (!firstCol) {
+				res += " ";
 			}
-			if (entities[entityName] == PqlEntityType::Stmt || entities[entityName] == PqlEntityType::Constant) {
-				value = std::to_string(it->second[i]);
-			}
-			else if ((entities[entityName] == PqlEntityType::Variable)) {
-				value = Entity::getVarName(it->second[i]);
-			}
-			else { //PqlEntityType::Procedure
-				value = Entity::getProcName(it->second[i]);
-			}
-
-			if (res == "") {
-				res = value;
-			}
-			else {
-				res += " " + value;
-			}
-
-			it++;
+			res += value;
+			firstCol = false;
 		}
 		resList.push_back(res);
 	}
@@ -60,9 +75,13 @@ std::list<std::string> PQLResultProjector::resolveTableToResults() {
 	return resList;
 }
 
-PQLResultProjector::PQLResultProjector(EvaluatedTable evTable, std::vector<std::string> columns) :
+PQLResultProjector::PQLResultProjector(
+	EvaluatedTable evTable,
+	std::vector<std::string> columns,
+	std::unordered_map<std::string, PqlEntityType> declarations) :
 	evaluatedTable(evTable),
-	columnsProjected(columns) {}
+	columnsProjected(columns),
+	declarations(declarations) {}
 
 PQLResultProjector::PQLResultProjector() {
 }
