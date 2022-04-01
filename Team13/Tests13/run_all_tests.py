437bfaf689f372a0eef545cb82a77c7df9784af0
Brandon Foong <63003202+brandonfoong@users.noreply.github.com>
2022-04-01 22:27:47 +0800

    Testing script improvements (#345)
    * Abstract out functions in test script
    
    * update test script
    
    * Port over changes to script
    
    * Small changes to test script
    
    * Update script, stop tracking .xml files

--- Team13/Tests13/run_all_tests.py
+++ Team13/Tests13/run_all_tests.py
@@ -16,7 +16,7 @@ VS_DEVENV = 'devenv.com'
 VS_2022_PATH = 'C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\'
 VS_2019_PATH = 'C:\\Program Files\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\'
 
-def main():
+def clean_and_build():
     # Figure out if we're using VS2019 or 2022
     VS_PATH = VS_2022_PATH if os.path.exists(VS_2022_PATH) else VS_2019_PATH
     
@@ -28,9 +28,20 @@ def main():
     subprocess.run([VS_PATH + VS_DEVENV, SLN_PATH, PROJECT_FLAG, AUTOTESTER_PROJ,
                      BUILD_FLAG, RELEASE_BUILD])
 
+def run_tests(subfolder):
+    # Make sure that the subfolder exists
+    cwd = os.getcwd()
+    subdirectory = os.path.join(cwd, subfolder)
+    if not os.path.exists(subdirectory) or not os.path.isdir(subdirectory):
+        return
+    
     # Run tests
+    print('=' * 40)
+    print(f'Running tests in: {subfolder}')
+    print('=' * 40)
+    
     txt_files = set()
-    for file in os.listdir():
+    for file in os.listdir(subdirectory):
         if file.endswith('.txt'):
             txt_files.add(file)
 
@@ -38,10 +49,10 @@ def main():
         if not file.endswith('_queries.txt'):
             continue
         root_name = file[:-12]
-        query_file = file
-        source_file = root_name + '_source.txt'
-        xml_file = root_name + '_out.xml'
-        if source_file not in txt_files:
+        query_file = os.path.join(subdirectory, root_name + '_queries.txt')
+        source_file = os.path.join(subdirectory, root_name + '_source.txt')
+        xml_file = os.path.join(subdirectory, root_name + '_out.xml')
+        if os.path.basename(source_file) not in txt_files:
             continue
         print(f'Running test set: {root_name}')
         subprocess.call([AUTOTESTER_PATH, source_file, query_file, xml_file])
@@ -53,6 +64,15 @@ def main():
                 + len(tree.findall('.//crash'))
             num_queries = len(tree.findall('.//query'))
             print(f'Number of errors: {num_errors} / {num_queries}')
+
+def main():
+    clean_and_build()
+    run_tests('./')
+    curr_dir = os.getcwd()
+    for sub in os.listdir():
+        if os.path.isdir(os.path.join(curr_dir, sub)):
+            run_tests(sub)
+
     
 if __name__ == '__main__':
     main()
